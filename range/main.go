d8863b4c713c8df4e76690921c8b10fe7e7a95f5
nikhil-dev-395 <nikhilw395@gmail.com>
2025-02-04 17:56:52 +0530

    range is completed

--- range/main.go
+++ range/main.go
@@ -6,11 +6,34 @@ func main() {
 	/*
 		we are going to see here how the `range` works in go
 	*/
-	fmt.Println("hii this is range")
-	nums := []int{6, 7, 8}
+	/* fmt.Println("hii this is range")
+	 nums := []int{6, 7, 8}
 	// here we are just iterating only
-	for i := 0; i < len(nums); i++ {
-		fmt.Println(nums[i])
+	 for i := 0; i < len(nums); i++ {
+	 	fmt.Println(nums[i])
+	 }
+
+	 range on array
+	 for index, num := range nums {
+	 	fmt.Println(index, num)
+	 }
+
+	 fmt.Println(nums)
+
+	*/
+
+	//  range on map = object(js)
+	/*
+		names := map[string]string{"name1": "nikhil", "name2": "arjun"}
+
+		for index, val := range names {
+			fmt.Println(index, val)
+		}
+	*/
+
+	// range on string
+
+	for index, val := range "nikhil" {
+		fmt.Println(index, string(val))
 	}
-	fmt.Println(nums)
 }
---
+++ readme.md
@@ -0,0 +1,9 @@
+# Golang Tutorial
+
+ ### i just made this repo for tracking my golang growth , so you can ignore this because this is for my personal use,
+
+thanks you
+
+if you want to also learn golang , you can check this playlist if coder's gyan
+
+golang playlist - [click here](https://www.youtube.com/watch?v=YmGp5Uzh4ag&list=PLXQpH_kZIxTWUe-Ee-DZEX5gfeoo4tHV6)
